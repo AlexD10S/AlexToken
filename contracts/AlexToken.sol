936e89a462e5d6ccf081743ce2636af6e43e0e77
AlexD10S <alexfraga10@gmail.com>
2018-11-29 15:39:04 +0000

    ERC20 token complete

--- contracts/AlexToken.sol
+++ contracts/AlexToken.sol
@@ -8,11 +8,19 @@ contract AlexToken {
     uint256 public totalSupply;
     //mapping in Solidity is key-value store
     mapping(address => uint256) public balanceOf;
+    //Account A, approve account B,C,D...
+    mapping(address => mapping(address => uint256)) public allowance;
 
     //We can listen Event, so be aware when it is triggered
     event Transfer(
         address indexed _from,
         address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
         uint256 _value
     );
 
@@ -33,6 +41,33 @@ contract AlexToken {
         //Must fire a transfer EVENT(see documentation ERC20)
         Transfer(msg.sender,_to,_value);
         
+        return true;
+    }
+
+    /*
+     * Allows _spender to withdraw from your account multiple times, up to the _value amount. 
+     * If this function is called again it overwrites the current allowance with _value.
+     */
+    function approve (address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender,_spender,_value);
+
+        return true;
+    }
+
+    //Like the function above, transfer, what with a third party(_from)
+    function transferFrom (address _from, address _to, uint256 _value) public returns (bool success){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        //Update the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        //Update the allowance
+        allowance[_from][msg.sender] -= _value;
+        //Fire the event
+        Transfer(_from,_to,_value);
+
         return true;
     }
 }
